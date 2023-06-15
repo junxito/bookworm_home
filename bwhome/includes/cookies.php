4085c48af641c8c372234983f267c7697a56f041
junxito <juanandreslopezjimenez@gmail.com>
2023-06-16 00:02:30 +0200

    cookies

--- bwhome/includes/cookies.php
+++ bwhome/includes/cookies.php
@@ -1,4 +1,4 @@
-<div class="cookie_box" id="cookie_box">
+<div class="cookie_box text-dark" id="cookie_box">
 	<img src="./assets/img/content/cookie.png" alt="">
 	<h1>Política Cookie</h1>
 	<p>Este sitio puede almacenar cookies en el sitio web y divulgar información de acuerdo con nuestra política de cookies <a href="./terminosLegales.php" class="link-primary">Leer más</a></p>
